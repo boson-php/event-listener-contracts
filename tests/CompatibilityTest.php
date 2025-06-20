2705112793a4930b9c9d4e49eb3e2ed160faf558
SerafimArts <nesk@xakep.ru>
2025-06-20 16:10:40 +0000

    Bump project version

---
+++ tests/CompatibilityTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Contracts\EventListener\Tests;
+
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Note: Changing the behavior of these tests is allowed ONLY when updating
+ *       a MAJOR version of the package.
+ */
+#[Group('boson-php/event-listener-contracts')]
+final class CompatibilityTest extends TestCase {}
